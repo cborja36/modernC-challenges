c8639eda52ef59b2f84988652f1d8756d54b7dd7
cborja36 <cborja36@gmail.com>
2023-08-18 20:24:58 +0200

    more elegant merge sort

--- challenge1.c
+++ challenge1.c
@@ -6,13 +6,18 @@
 #include <string.h>
 #include <time.h>
 
+/**********************/
+/***** Merge Sort *****/
+/**********************/
+
 void copy(double working_arr[], double arr[], size_t l, size_t r) {
   for (size_t i = 0; i < r - l; ++i) {
     arr[l + i] = working_arr[i];
   }
 }
 
-void merge(double arr[], size_t l, size_t m, size_t r, double working_arr[]) {
+void merge(double arr[], size_t l, size_t m, size_t r) {
+  double working_arr[r - l];
   size_t i = l, j = m;
   for (size_t k = 0; k < r - l; ++k) {
     if (i < m && (j >= r || arr[i] <= arr[j])) {
@@ -24,15 +29,19 @@ void merge(double arr[], size_t l, size_t m, size_t r, double working_arr[]) {
   copy(working_arr, arr, l, r);
 }
 
-void MergeSort(double arr[], size_t l, size_t r, double working_arr[]) {
+void MergeSort(double arr[], size_t l, size_t r) {
   if (r - l <= 1) return;
 
   size_t m = (l + r) / 2;
-  MergeSort(arr, l, m, working_arr);
-  MergeSort(arr, m, r, working_arr);
+  MergeSort(arr, l, m);
+  MergeSort(arr, m, r);
 
-  merge(arr, l, m, r, working_arr);
+  merge(arr, l, m, r);
 }
+
+/******************************/
+/***** Auxiliar functions *****/
+/******************************/
 
 bool is_sorted(double arr[], size_t n) {
   for (size_t i = 0; i < n - 1; ++i) {
@@ -64,7 +73,6 @@ int main(int argc, char *argv[]) {
 
   puts("Initializing array...");
   double arr[N];
-  double working_arr[N];
 
   for (int i = 0; i < 10; ++i) {
     puts("Filling array with random values...");
@@ -78,7 +86,7 @@ int main(int argc, char *argv[]) {
     }
 
     puts("Sorting array...");
-    MergeSort(arr, 0, N, working_arr);
+    MergeSort(arr, 0, N);
     if (!is_sorted(arr, N)) {
       printf("Error: array is not sorted\n");
       return EXIT_FAILURE;
